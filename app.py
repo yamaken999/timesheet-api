227a6beb5da138323366ba52a1080e5b411631d9
kenichi.yamamoto <kenichi.yamamoto@accenture.com>
2025-06-19 14:58:23 +0900

    app.py　環境設定ファイル追加

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-echo 'hello world'
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Timesheet API is running"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
